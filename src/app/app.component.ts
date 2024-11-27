6969229f9906496cd8747ffd93f542326832c074
klemenp <klemen.pravdic@arnes.si>
2024-11-27 14:06:28 +0100

    rm package-lock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { interval, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [JsonPipe, AsyncPipe],
+  template: `
+    <h1>Resource 1</h1>
+    <pre>{{ todoR.value() | json }}</pre>
+    <h1>Resource 2</h1>
+    <pre>{{ todoObs$ | async | json }}</pre>
+  `,
 })
 export class AppComponent {
-  title = 'ng19';
+  #http = inject(HttpClient);
+
+  todoObs$ = this.fetchTodo$();
+
+  todoR = rxResource({
+    loader: () => {
+      return this.fetchTodo$();
+    },
+  });
+
+  fetchTodo$() {
+    return interval(2000) // 2 sec
+      .pipe(
+        switchMap(() =>
+          this.#http.get(
+            'https://jsonplaceholder.typicode.com/todos/' +
+              Math.floor(Math.random() * 100)
+          )
+        )
+      );
+  }
 }
